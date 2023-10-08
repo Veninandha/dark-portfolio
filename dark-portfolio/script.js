ed786f7b4f9344953d3d3170de8d7840cc2ef74a
Veninandha <147284246+Veninandha@users.noreply.github.com>
2023-10-08 06:28:37 +0530

    Delete dark-portfolio directory

--- dark-portfolio/script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript code to toggle dark mode
-const darkModeToggle = document.getElementById("darkModeToggle");
-const body = document.body;
-
-darkModeToggle.addEventListener("change", () => {
-  body.classList.toggle("dark-mode");
-});
